6e15d74d54487d125eda18829260c9dfcd1a769c
newen854-max <newen854@gmail.com>
2026-06-14 05:55:59 +0300

    Create Incidents.js

---
+++ src/screens/Incidents.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Incidents() {
+  return (
+    <View style={{ padding: 20 }}>
+      <Text style={{ fontSize: 20 }}>Incident Management</Text>
+      <Text>List + Create + Edit (Next Module)</Text>
+    </View>
+  );
+}
