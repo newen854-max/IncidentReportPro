5739c6cdb0da1bec9e1d7bb1278a9ac5f07d40ce
newen854-max <newen854@gmail.com>
2026-06-14 05:57:40 +0300

    Create Settings.js

---
+++ src/screens/Settings.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Settings() {
+  return (
+    <View style={{ padding: 20 }}>
+      <Text style={{ fontSize: 20 }}>Settings</Text>
+    </View>
+  );
+}
