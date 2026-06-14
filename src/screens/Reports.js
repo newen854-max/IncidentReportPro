7ad52db1d587898ebc5cea7122b971780efdb994
newen854-max <newen854@gmail.com>
2026-06-14 05:57:16 +0300

    Create Reports.js

---
+++ src/screens/Reports.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Reports() {
+  return (
+    <View style={{ padding: 20 }}>
+      <Text style={{ fontSize: 20 }}>Reports (PDF / Excel)</Text>
+    </View>
+  );
+}
